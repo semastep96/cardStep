665c7b7a5330e6a87f27467cfe1a07831fe800bd
Степанов Семен <stepanovsm@modeltech.ru>
2025-07-22 23:09:27 +0300

    fix pwa install

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  base: '/cardStep/',
+  base: './',
   build: {
     outDir: 'docs',
   },
