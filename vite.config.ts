1c8728fb86d0580639ad712dd06645d63f2aca7d
Степанов Семен <stepanovsm@modeltech.ru>
2025-07-22 22:40:31 +0300

    fix docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
+  base: './',
+  build: {
+    outDir: 'docs',
+  },
 });
