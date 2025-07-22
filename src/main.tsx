d4d03355d1a595cb9eb77942cb1c28b6947a5a6b
Степанов Семен <stepanovsm@modeltech.ru>
2025-07-22 23:03:54 +0300

    fix pwa install

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('./sw.js')
       .then(reg => console.log('SW registered:', reg.scope))
       .catch(err => console.error('SW registration failed:', err));
   });
