5d06411eafcb2d2a73e0229a576265caea58c787
Степанов Семен <stepanovsm@modeltech.ru>
2025-07-26 13:50:04 +0300

    build

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'app-cache-v5';
+const CACHE_NAME = 'app-cache-v4';
 const CORE_ASSETS = ['/cardStep', '/cardStep/manifest.json', '/cardStep/scanner-beep.mp3'];
 
 // При установке кешируем только «ядро» приложения
